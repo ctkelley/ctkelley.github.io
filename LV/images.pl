a7c0c9b70d5a0e1ef4920c6d161e9dc610fb1091
Tim Kelley <ctk@ncsu.edu>
2025-04-11 18:41:22 -0400

    More www stuff

---
+++ LV/images.pl
@@ -0,0 +1,8 @@
+# LaTeX2HTML 95.1 (Fri Jan 20 1995)
+# Associate image original text (scrambled) with physical files.
+
+$key = q/{_inline}$F_N${_inline}/;
+$cached_env_img{$key} ='<IMG BORDER=0 ALIGN=MIDDLE ALT="" SRC="img4.gif">'; 
+
+1;
+
